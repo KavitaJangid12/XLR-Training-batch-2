10891f203e015cf28f5ddc85029ab876645633a5
KavitaJangid12 <kavitajangid1205@gmail.com>
2025-01-06 07:39:45 +0530

    Username and Password

---
+++ Core Java/Demo006.java
@@ -0,0 +1,10 @@
+public class Demo006{
+    public static void main(String[] args) {
+        String username = "kavita";
+        String pwd = "kavita12";
+        while((username == "kavita") && (pwd == "kavita12")){
+            System.out.println("Welcome Kavita");
+            break;
+        }
+    }
+}
