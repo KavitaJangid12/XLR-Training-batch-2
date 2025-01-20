ee6cfeb2c31310fcc01078acd582f6ff4df6cd0b
KavitaJangid12 <kavitajangid1205@gmail.com>
2025-01-20 14:02:28 +0530

    Array

---
+++ Core Java/Demo029.java
@@ -0,0 +1,7 @@
+public class Demo029{
+    public static void main(String[] args) {
+        String[] arr = {"Tharuni","Praharshini","Jhansi","Priyanka","Anupama","Kasturi","Jaitra","Devansh","Khush","Akshatha","Shirin"};
+        System.out.println(arr);
+
+    }
+}
