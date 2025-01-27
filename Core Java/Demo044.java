d2a46f8bb47279d6de305b30937de547ac51a600
KavitaJangid12 <kavitajangid1205@gmail.com>
2025-01-27 07:54:24 +0530

    Swapping using 3rd variable

---
+++ Core Java/Demo044.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Demo044 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        int a = scanner.nextInt();
+        System.out.print("Enter second number: ");
+        int b = scanner.nextInt();
+        System.out.println("Before swapping: a = " + a + ", b = " + b);
+        int temp = a;
+        a = b;
+        b = temp;
+        System.out.println("After swapping: a = " + a + ", b = " + b);
+    }
+}
