97c8425071a1b16dd45d4239de8393cfba754bbb
KavitaJangid12 <kavitajangid1205@gmail.com>
2025-01-29 09:43:39 +0530

    Count of digits

---
+++ Core Java/Demo047.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Demo047 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number: ");
+        int num = sc.nextInt();
+        int count = 0;
+        if (num == 0) {
+            count = 1;
+
+        } else {
+            while (num != 0) {
+                num = num / 10;
+                count++;
+            }
+        }
+        System.out.println("the number has"+count+"digits");
+
+    }
+}
